648490714bc72679535370ba0ac658d166e08d7a
Karen White <31664136+karen-white@users.noreply.github.com>
2019-03-08 08:33:53 -0600

    clean up routes

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,13 @@ const router = express.Router();
 let webhook = "";
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { webhook: webhook });
-});
-
-router.post('/', (req, res) => {
-  webhook = JSON.stringify(req.body);
-  res.sendStatus(200);
+res.render('index');  
 });
 
 router.post('/webhook', (req, res) => {
   webhook = JSON.stringify(req.body);
   res.sendStatus(200);
+  res.render('index', { webhook: webhook });
 });
 
 module.exports = router;
